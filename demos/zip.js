7f0f959865162e755710598151b3ec0f86e9d325
Gildas Lormeau <gildas.lormeau@gmail.com>
2011-09-17 21:28:43 +0200

    crc is now computed while deflating

--- demos/zip.js
+++ demos/zip.js
@@ -33,7 +33,7 @@
 		};
 	}
 
-	// BlobReader
+	// Readers
 
 	function BlobReader(blob) {
 		var that = this;
@@ -56,31 +56,23 @@
 			callback(blobSlice(blob, index, length));
 		}
 
-		function getBlob() {
-			return blob;
-		}
-
 		that.size = 0;
 		that.init = init;
 		that.readBlob = readBlob;
 		that.readUint8Array = readUint8Array;
-		that.getBlob = getBlob;
 	}
-
-	// HttpRangeReader
 
 	function HttpRangeReader(url) {
 		var that = this;
 
 		function init(callback, onerror) {
 			var request = new XMLHttpRequest();
-			url = initUrl;
 			request.addEventListener("load", function() {
 				that.size = Number(request.getResponseHeader("Content-Length"));
 				if (request.getResponseHeader("Accept-Ranges") == "bytes")
 					callback();
 				else
-					onerror("HTTP Range not supported!");
+					onerror("HTTP Range not supported.");
 			}, false);
 			request.addEventListener("error", onerror, false);
 			request.open("HEAD", url);
@@ -113,17 +105,13 @@
 			}, onerror);
 		}
 
-		function getBlob() {
-			throw "HttpRangeReader does not support getBlob method.";
-		}
-
 		that.size = 0;
 		that.init = init;
 		that.readBlob = readBlob;
 		that.readUint8Array = readUint8Array;
 	}
 
-	// FileWriter
+	// Writers
 
 	function FileWriter(file) {
 		var writer, that = this;
@@ -135,12 +123,6 @@
 			}, onerror);
 		}
 
-		function appendArrayBuffer(arrayBuffer, callback, onerror) {
-			var blobBuilder = new BlobBuilder();
-			blobBuilder.append(arrayBuffer);
-			appendBlob(blobBuilder.getBlob(), callback, onerror);
-		}
-
 		function appendBlob(blob, callback, onerror) {
 			writer.onwrite = function() {
 				writer.onwrite = null;
@@ -148,6 +130,12 @@
 			};
 			writer.onerror = onerror;
 			writer.write(blob);
+		}
+
+		function appendArrayBuffer(arrayBuffer, callback, onerror) {
+			var blobBuilder = new BlobBuilder();
+			blobBuilder.append(arrayBuffer);
+			appendBlob(blobBuilder.getBlob(), callback, onerror);
 		}
 
 		function getBlob() {
@@ -175,8 +163,6 @@
 		that.getBlob = getBlob;
 		that.seek = seek;
 	}
-
-	// BlobWriter
 
 	function BlobWriter() {
 		var blobBuilder, that = this;
@@ -262,12 +248,6 @@
 		return str;
 	}
 
-	function createZipReader(reader, callback, onerror) {
-		reader.init(function() {
-			callback(createZipReaderCore(reader, onerror));
-		}, onerror);
-	}
-
 	function createZipReaderCore(reader, onerror) {
 		var worker, CHUNK_SIZE = 512 * 1024;
 
@@ -293,8 +273,13 @@
 							var fileReader = new FileReader(), index = chunkIndex * CHUNK_SIZE, size = data.size;
 
 							if (onprogress)
-								onprogress(index, size);
+								onprogress({
+									inflate : true,
+									index : index,
+									max : size
+								});
 							if (index < size) {
+								fileReader.onerror = onerror;
 								fileReader.onload = function(event) {
 									worker.postMessage({
 										append : true,
@@ -413,7 +398,48 @@
 		};
 	}
 
+	function createZipReader(reader, callback, onerror) {
+		reader.init(function() {
+			callback(createZipReaderCore(reader, onerror));
+		}, onerror);
+	}
+
 	// ZipWriter
+
+	function Crc32() {
+		var crc = -1, that = this, table = [ 0, 1996959894, 3993919788, 2567524794, 124634137, 1886057615, 3915621685, 2657392035, 249268274, 2044508324,
+				3772115230, 2547177864, 162941995, 2125561021, 3887607047, 2428444049, 498536548, 1789927666, 4089016648, 2227061214, 450548861, 1843258603,
+				4107580753, 2211677639, 325883990, 1684777152, 4251122042, 2321926636, 335633487, 1661365465, 4195302755, 2366115317, 997073096, 1281953886,
+				3579855332, 2724688242, 1006888145, 1258607687, 3524101629, 2768942443, 901097722, 1119000684, 3686517206, 2898065728, 853044451, 1172266101,
+				3705015759, 2882616665, 651767980, 1373503546, 3369554304, 3218104598, 565507253, 1454621731, 3485111705, 3099436303, 671266974, 1594198024,
+				3322730930, 2970347812, 795835527, 1483230225, 3244367275, 3060149565, 1994146192, 31158534, 2563907772, 4023717930, 1907459465, 112637215,
+				2680153253, 3904427059, 2013776290, 251722036, 2517215374, 3775830040, 2137656763, 141376813, 2439277719, 3865271297, 1802195444, 476864866,
+				2238001368, 4066508878, 1812370925, 453092731, 2181625025, 4111451223, 1706088902, 314042704, 2344532202, 4240017532, 1658658271, 366619977,
+				2362670323, 4224994405, 1303535960, 984961486, 2747007092, 3569037538, 1256170817, 1037604311, 2765210733, 3554079995, 1131014506, 879679996,
+				2909243462, 3663771856, 1141124467, 855842277, 2852801631, 3708648649, 1342533948, 654459306, 3188396048, 3373015174, 1466479909, 544179635,
+				3110523913, 3462522015, 1591671054, 702138776, 2966460450, 3352799412, 1504918807, 783551873, 3082640443, 3233442989, 3988292384, 2596254646,
+				62317068, 1957810842, 3939845945, 2647816111, 81470997, 1943803523, 3814918930, 2489596804, 225274430, 2053790376, 3826175755, 2466906013,
+				167816743, 2097651377, 4027552580, 2265490386, 503444072, 1762050814, 4150417245, 2154129355, 426522225, 1852507879, 4275313526, 2312317920,
+				282753626, 1742555852, 4189708143, 2394877945, 397917763, 1622183637, 3604390888, 2714866558, 953729732, 1340076626, 3518719985, 2797360999,
+				1068828381, 1219638859, 3624741850, 2936675148, 906185462, 1090812512, 3747672003, 2825379669, 829329135, 1181335161, 3412177804, 3160834842,
+				628085408, 1382605366, 3423369109, 3138078467, 570562233, 1426400815, 3317316542, 2998733608, 733239954, 1555261956, 3268935591, 3050360625,
+				752459403, 1541320221, 2607071920, 3965973030, 1969922972, 40735498, 2617837225, 3943577151, 1913087877, 83908371, 2512341634, 3803740692,
+				2075208622, 213261112, 2463272603, 3855990285, 2094854071, 198958881, 2262029012, 4057260610, 1759359992, 534414190, 2176718541, 4139329115,
+				1873836001, 414664567, 2282248934, 4279200368, 1711684554, 285281116, 2405801727, 4167216745, 1634467795, 376229701, 2685067896, 3608007406,
+				1308918612, 956543938, 2808555105, 3495958263, 1231636301, 1047427035, 2932959818, 3654703836, 1088359270, 936918000, 2847714899, 3736837829,
+				1202900863, 817233897, 3183342108, 3401237130, 1404277552, 615818150, 3134207493, 3453421203, 1423857449, 601450431, 3009837614, 3294710456,
+				1567103746, 711928724, 3020668471, 3272380065, 1510334235, 755167117 ];
+
+		that.append = function(data) {
+			var offset;
+			for (offset = 0; offset < data.length; offset++)
+				crc = (crc >>> 8) ^ table[(crc ^ data[offset]) & 0xFF];
+		};
+
+		that.get = function() {
+			return crc ^ (-1);
+		};
+	}
 
 	function encodeUTF8(argString) {
 		if (argString === null || typeof argString === "undefined") {
@@ -459,22 +485,13 @@
 		return array;
 	}
 
-	function createZipWriter(writer, dontDeflate, callback, onerror) {
-		writer.init(function() {
-			callback(createZipWriterCore(writer, dontDeflate, onerror));
-		}, onerror);
-	}
-
 	function createZipWriterCore(writer, dontDeflate, onerror) {
-		var worker, crcWorker, files = [], filenames = [], datalength = 0, CHUNK_SIZE = 512 * 1024;
+		var worker, files = [], filenames = [], datalength = 0, CHUNK_SIZE = 512 * 1024;
 
 		function terminate(callback, message) {
 			if (worker)
 				worker.terminate();
-			if (crcWorker)
-				crcWorker.terminate();
 			worker = null;
-			crcWorker = null;
 			if (callback)
 				callback(message);
 		}
@@ -485,25 +502,35 @@
 
 		return {
 			add : function(name, reader, options, onend, onprogress) {
-				var filename = getBytes(encodeUTF8(name)), compressedDataSize = 0, chunkIndex = 0;
+				var filename = getBytes(encodeUTF8(name)), compressedDataSize = 0, chunkIndex, crc32 = new Crc32();
 
-				function stepDeflate(level) {
-					var index = chunkIndex * CHUNK_SIZE, size = reader.size;
-					if (onprogress)
-						onprogress(index, size);
-					if (index < size) {
-						reader.readUint8Array(index, Math.min(CHUNK_SIZE, size - index), function(data) {
-							worker.postMessage({
-								append : true,
-								data : new Uint8Array(data),
-								level : level
-							});
-							chunkIndex++;
-						});
-					} else
-						worker.postMessage({
-							flush : true
-						});
+				function writeFileMetadata() {
+					var date = new Date(), header = getDataHelper(26), data = getDataHelper(30 + filename.length);
+					filenames.push(name);
+					files[name] = {
+						headerArray : header.array,
+						directory : options.directory,
+						filename : filename,
+						offset : datalength
+					};
+					header.view.setUint32(0, 0x0a000008);
+					if (!dontDeflate)
+						header.view.setUint16(4, 0x0800);
+					header.view.setUint16(6, (((date.getHours() << 6) | date.getMinutes()) << 5) | date.getSeconds() / 2, true);
+					header.view.setUint16(8, ((((date.getFullYear() - 1980) << 4) | (date.getMonth() + 1)) << 5) | date.getDate(), true);
+					header.view.setUint32(10, crc32.get(), true);
+					header.view.setUint32(14, compressedDataSize, true);
+					header.view.setUint32(18, reader.size, true);
+					header.view.setUint16(22, filename.length, true);
+					data.view.setUint32(0, 0x504b0304);
+					data.array.set(header.array, 4);
+					data.array.set(filename, 30);
+					writer.seek(datalength, function() {
+						writer.appendArrayBuffer(data.buffer, function() {
+							datalength += data.buffer.byteLength + compressedDataSize;
+							onend();
+						}, onWriteError);
+					}, onWriteError);
 				}
 
 				function appendBlob(blob, callback) {
@@ -511,6 +538,26 @@
 					writer.appendBlob(blob, function() {
 						callback();
 					}, onWriteError);
+				}
+
+				function stepDeflate(level) {
+					var index = chunkIndex * CHUNK_SIZE, size = reader.size;
+					if (onprogress)
+						onprogress(index, size);
+					if (index < size) {
+						reader.readUint8Array(index, Math.min(CHUNK_SIZE, size - index), function(data) {
+							crc32.append(data);
+							worker.postMessage({
+								append : true,
+								data : data,
+								level : level
+							});
+							chunkIndex++;
+						}, onerror);
+					} else
+						worker.postMessage({
+							flush : true
+						});
 				}
 
 				function deflate(level, callback) {
@@ -532,46 +579,34 @@
 					}
 					worker = new Worker(WORKER_SCRIPTS_PATH + "deflate.js");
 					worker.addEventListener("message", onmessage, false);
+					chunkIndex = 0;
 					stepDeflate(level);
 				}
 
-				function onmessage(event) {
-					var crc = event.data, date = new Date(), header = getDataHelper(26), data = getDataHelper(30 + filename.length);
-					crcWorker.removeEventListener("message", onmessage, false);
-					crcWorker.terminate();
-					crcWorker = null;
-					filenames.push(name);
-					files[name] = {
-						headerArray : header.array,
-						directory : options.directory,
-						filename : filename,
-						offset : datalength
-					};
-					header.view.setUint32(0, 0x0a000008);
-					if (!dontDeflate)
-						header.view.setUint16(4, 0x0800);
-					header.view.setUint16(6, (((date.getHours() << 6) | date.getMinutes()) << 5) | date.getSeconds() / 2, true);
-					header.view.setUint16(8, ((((date.getFullYear() - 1980) << 4) | (date.getMonth() + 1)) << 5) | date.getDate(), true);
-					header.view.setUint32(10, crc, true);
-					header.view.setUint32(14, compressedDataSize, true);
-					header.view.setUint32(18, reader.size, true);
-					header.view.setUint16(22, filename.length, true);
-					data.view.setUint32(0, 0x504b0304);
-					data.array.set(header.array, 4);
-					data.array.set(filename, 30);
-					writer.seek(datalength, function() {
-						writer.appendArrayBuffer(data.buffer, function() {
-							datalength += data.buffer.byteLength + compressedDataSize;
-							onend();
-						}, onWriteError);
-					}, onWriteError);
+				function stepCopy(callback) {
+					var index = chunkIndex * CHUNK_SIZE, size = reader.size;
+					if (onprogress)
+						onprogress(index, size);
+					if (index < size)
+						reader.readBlob(index, Math.min(CHUNK_SIZE, size - index), function(blob) {
+							appendBlob(blob, function() {
+								var fileReader = new FileReader();
+								fileReader.onerror = onerror;
+								fileReader.onload = function(event) {
+									crc32.append(new Uint8Array(event.target.result));
+									chunkIndex++;
+									stepCopy(callback);
+								};
+								fileReader.readAsArrayBuffer(blob);
+							});
+						}, onerror);
+					else
+						callback();
 				}
 
-				function writeFileMetadata() {
-					crcWorker = new Worker(WORKER_SCRIPTS_PATH + "crc32.js");
-					crcWorker.addEventListener("message", onmessage, false);
-					// TODO : buffered read instead of getBlob call in order to support HttpRangeReader
-					crcWorker.postMessage(reader.getBlob());
+				function copy(callback) {
+					chunkIndex = 0;
+					stepCopy(callback);
 				}
 
 				name = name.trim();
@@ -580,11 +615,9 @@
 				options = options || {};
 				writer.seek(datalength + 30 + filename.length, function() {
 					reader.init(function() {
-						if (dontDeflate) {
-							// TODO : buffered read instead of getBlob call in order to support HttpRangeReader
-							writer.appendBlob(reader.getBlob());
-							writeFileMetadata();
-						} else
+						if (dontDeflate)
+							copy(writeFileMetadata);
+						else
 							deflate(options.level, writeFileMetadata);
 					}, onerror);
 				}, onWriteError);
@@ -618,6 +651,12 @@
 				}, onWriteError);
 			}
 		};
+	}
+
+	function createZipWriter(writer, dontDeflate, callback, onerror) {
+		writer.init(function() {
+			callback(createZipWriterCore(writer, dontDeflate, onerror));
+		}, onerror);
 	}
 
 	obj.zip = {
