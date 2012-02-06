b54c00ee26d3fb3f5fbc532efeae1d13d5850b7b
Gildas Lormeau <gildas.lormeau@gmail.com>
2012-02-06 21:16:26 +0100

    added simple test: read a zip with an uncompressed entry

---
+++ WebContent/tests/test10.js
@@ -0,0 +1,34 @@
+var URL = "lorem_store.zip";
+
+var zipFs = new zip.fs.FS();
+
+function onerror(message) {
+	console.error(message);
+}
+
+function zipImportedZip(callback) {
+	var directory = zipFs.root.addDirectory("import");
+	directory.importHttpContent(URL, false, function() {
+		zipFs.exportBlob(callback, null, onerror);
+	}, onerror);
+}
+
+function unzipBlob(blob, callback) {
+	zipFs.importBlob(blob, function() {
+		var directory = zipFs.root.getChildByName("import");
+		var firstEntry = directory.children[0];
+		firstEntry.getText(callback);
+	}, onerror);
+}
+
+function logText(text) {
+	console.log(text);
+	console.log("--------------");
+}
+
+zip.workerScriptsPath = "../";
+zipImportedZip(function(zippedBlob) {
+	unzipBlob(zippedBlob, function(unzippedText) {
+		logText(unzippedText);
+	});
+});
