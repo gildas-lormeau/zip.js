f93037957b59be8da27e029161af71ac870ebf82
Gildas Lormeau <gildas.lormeau@gmail.com>
2012-02-08 00:21:19 +0100

    removed unused error messages

--- WebContent/deflate.js
+++ WebContent/deflate.js
@@ -430,12 +430,12 @@
 	// 2
 	"stream end", // Z_STREAM_END 1
 	"", // Z_OK 0
-	"file error", // Z_ERRNO (-1)
+	"", // Z_ERRNO (-1)
 	"stream error", // Z_STREAM_ERROR (-2)
 	"data error", // Z_DATA_ERROR (-3)
-	"insufficient memory", // Z_MEM_ERROR (-4)
+	"", // Z_MEM_ERROR (-4)
 	"buffer error", // Z_BUF_ERROR (-5)
-	"incompatible version",// Z_VERSION_ERROR (-6)
+	"",// Z_VERSION_ERROR (-6)
 	"" ];
 
 	// block not completed, need more input or more output
