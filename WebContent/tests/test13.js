48dc2439855775610399823fd7ca20fddffac761
Gildas Lormeau <gildas.lormeau@gmail.com>
2012-09-15 01:17:16 +0200

    removed BlobBuilder use from tests

--- WebContent/tests/test13.js
+++ WebContent/tests/test13.js
@@ -1,6 +1,5 @@
 var TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
 var requestFileSystem = window.webkitRequestFileSystem || window.mozRequestFileSystem || window.msRequestFileSystem || window.requestFileSystem;
-var BlobBuilder = window.WebKitBlobBuilder || window.MozBlobBuilder || window.MsBlobBuilder || window.BlobBuilder;
 var filesystem, zipFs = new zip.fs.FS();
 
 function onerror(message) {
@@ -49,13 +48,13 @@ function initFileSystem(callback) {
 		create : true
 	}, function(fileEntry) {
 		fileEntry.createWriter(function(writer) {
-			var blobBuilder = new BlobBuilder();
-			blobBuilder.append(TEXT_CONTENT);
 			writer.onwrite = function() {
 				callback(fileEntry);
 			};
 			writer.onerror = onerror;
-			writer.write(blobBuilder.getBlob());
+			writer.write(new Blob([ TEXT_CONTENT ], {
+				type : "text/plain"
+			}));
 		}, onerror);
 	}, onerror);
 }
