e5d10d8fc84d2221eda59139de1e3f32a9993946
Gildas Lormeau <gildas.lormeau@gmail.com>
2013-03-05 16:01:28 +0100

    fixed issue #40 (thanks to @Jacob-Christian-Munch-Andersen)

--- WebContent/zip.js
+++ WebContent/zip.js
@@ -46,7 +46,7 @@
 	try {
 		appendABViewSupported = new Blob([ getDataHelper(0).view ]).size == 0;
 	} catch (e) {
-	}	
+	}
 
 	function Crc32() {
 		var crc = -1, that = this;
@@ -591,29 +591,8 @@
 		return out;
 	}
 
-	function decodeUTF8(str_data) {
-		var tmp_arr = [], i = 0, ac = 0, c1 = 0, c2 = 0, c3 = 0;
-
-		str_data += '';
-
-		while (i < str_data.length) {
-			c1 = str_data.charCodeAt(i);
-			if (c1 < 128) {
-				tmp_arr[ac++] = String.fromCharCode(c1);
-				i++;
-			} else if (c1 > 191 && c1 < 224) {
-				c2 = str_data.charCodeAt(i + 1);
-				tmp_arr[ac++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
-				i += 2;
-			} else {
-				c2 = str_data.charCodeAt(i + 1);
-				c3 = str_data.charCodeAt(i + 2);
-				tmp_arr[ac++] = String.fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-				i += 3;
-			}
-		}
-
-		return tmp_arr.join('');
+	function decodeUTF8(string) {
+		return decodeURIComponent(escape(string));
 	}
 
 	function getString(bytes) {
@@ -778,26 +757,7 @@
 	// ZipWriter
 
 	function encodeUTF8(string) {
-		var n, c1, enc, utftext = [], start = 0, end = 0, stringl = string.length;
-		for (n = 0; n < stringl; n++) {
-			c1 = string.charCodeAt(n);
-			enc = null;
-			if (c1 < 128)
-				end++;
-			else if (c1 > 127 && c1 < 2048)
-				enc = String.fromCharCode((c1 >> 6) | 192) + String.fromCharCode((c1 & 63) | 128);
-			else
-				enc = String.fromCharCode((c1 >> 12) | 224) + String.fromCharCode(((c1 >> 6) & 63) | 128) + String.fromCharCode((c1 & 63) | 128);
-			if (enc != null) {
-				if (end > start)
-					utftext += string.slice(start, end);
-				utftext += enc;
-				start = end = n + 1;
-			}
-		}
-		if (end > start)
-			utftext += string.slice(start, stringl);
-		return utftext;
+		return unescape(encodeURIComponent(string));
 	}
 
 	function getBytes(str) {
