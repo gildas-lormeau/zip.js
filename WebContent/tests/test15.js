4bfce033e6c524df5501d11c6902c9a7ed0fc112
Duan Yao <duanyao@ustc.edu>
2014-11-08 19:07:01 +0800

    Improve parallel reads test (test15)

--- WebContent/tests/test15.js
+++ WebContent/tests/test15.js
@@ -1,4 +1,4 @@
-var TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
+"use strict";
 
 function onerror(message) {
 	console.error(message);
@@ -25,34 +25,67 @@ function zipBlobs(blobs, callback) {
 function unzipBlob(blob) {
 	zip.createReader(new zip.BlobReader(blob), function(zipReader) {
 		zipReader.getEntries(function(entries) {
-			var i;
-			for (i = 0; i < entries.length; i++)
-				entries[i].getData(new zip.TextWriter(), function(text) {
-					logText(text);
-				});
+			entries.forEach(readEntry);
 		});
 	}, onerror);
+
+	function readEntry(ent, i) {
+		var lastLogPos = 0;
+		ent.getData(new zip.BlobWriter(), function onload(blob) {
+			console.log('finished:' + ent.filename + ', size:' + blob.size);
+			compareResult(blob, i);
+		}, function onprogress(loaded, size) {
+			if (loaded - lastLogPos > 100 * 1024) { // limit progress log
+				console.log('onprogress:' + ent.filename + ', loaded:' + loaded + ', size:' + size);
+				lastLogPos = loaded;
+			}
+		},
+		true); //check crc32
+	}
 }
 
-function getBlob() {
-	return new Blob([ TEXT_CONTENT ], {
-		type : "text/plain"
-	});
+function compareResult(result, index) {
+	var fr1 = new FileReader(), fr2 = new FileReader(), loadCount = 0;
+	fr1.readAsArrayBuffer(blobs[index].blob);
+	fr2.readAsArrayBuffer(result);
+	fr1.onload = fr2.onload = function onload() {
+		if (++loadCount === 2) {
+			var a1 = new Float64Array(fr1.result), a2 = new Float64Array(fr2.result);
+			if (a1.length !== a2.length)
+				return fail();
+			for (var i = 0, n = a1.length; i < n; i++) {
+				if (a1[i] !== a2[i])
+					return fail();
+			}
+			console.log('compareResult OK at: ' + blobs[index].name);
+		}
+	};
+	function fail() {
+		console.error('Error: compareBlobs failed at: ' + blobs[index].name);
+	}
 }
 
-function logText(text) {
-	console.log(text);
-	console.log("--------------");
+function getBlob(size) {
+	var data = new Float64Array(Math.floor(size / 8));
+	var rand = Math.random;
+	for(var i = 0, n = data.length; i < n; i++)
+		data[i] = rand();
+	return new Blob([data]);
 }
 
-zip.workerScriptsPath = "../";
-
+var MB = 1024 * 1024;
 var blobs = [ {
-	name : "lorem1.txt",
-	blob : getBlob()
+	name : "b1",
+	blob : getBlob(3.5 * MB)
 }, {
-	name : "lorem2.txt",
-	blob : getBlob()
+	name : "b2",
+	blob : getBlob(5.2 * MB)
+} , {
+	name : "b3",
+	blob : getBlob(4.7 * MB)
+} , {
+	name : "b4",
+	blob : getBlob(2.8 * MB)
 } ];
 
 zipBlobs(blobs, function(zippedBlob) {
