14d85a6d6bbaf3673429897ef6383ea4ddfcdb69
Gildas <gildas.lormeau@gmail.com>
2021-01-11 23:57:45 +0100

    compress standalone scripts

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rollup lib/zip-fs.js --file dist/zip-fs.js --format umd --name "zip"
 
 rollup lib/zip.js --file dist/zip.min.js --format umd --name "zip" --plugin terser
 rollup lib/zip-fs.js --file dist/zip-fs.min.js --format umd --name "zip" --plugin terser
-terser lib/z-worker.js > dist/z-worker.js
-terser lib/deflate.js > dist/deflate.js
-terser lib/inflate.js > dist/inflate.js
-terser lib/crypto.js > dist/crypto.js
+terser lib/z-worker.js > dist/z-worker.js --compress --mangle
+terser lib/deflate.js > dist/deflate.js --compress --mangle
+terser lib/inflate.js > dist/inflate.js --compress --mangle
+terser lib/crypto.js > dist/crypto.js --compress --mangle
