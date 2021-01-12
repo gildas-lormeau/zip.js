e61b35223eb6f6a05fbf75f846931566961e1396
Gildas <gildas.lormeau@gmail.com>
2021-01-12 19:38:35 +0100

    add shims for pako and zlib

--- build.sh
+++ build.sh
@@ -9,3 +9,6 @@ terser lib/z-worker.js > dist/z-worker.js --compress --mangle
 terser lib/deflate.js > dist/deflate.js --compress --mangle
 terser lib/inflate.js > dist/inflate.js --compress --mangle
 terser lib/crypto.js > dist/crypto.js --compress --mangle
+
+rollup lib/z-worker-pako.js --file dist/z-worker-pako.js --format umd --plugin terser
+rollup lib/z-worker-zlib.js --file dist/z-worker-zlib.js --format umd --plugin terser
