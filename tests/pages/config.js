7da826a597c8d4dd34bafbe674f1de4531781473
Gildas <gildas.lormeau@gmail.com>
2021-01-17 15:49:24 +0100

    remove obsolete comment

--- tests/pages/config.js
+++ tests/pages/config.js
@@ -6,16 +6,3 @@ zip.configure({
 	useWebWorkers: true,
 	workerScriptsPath: "../../dist/"
 });
-
-// to test third party deflate implementations, comment out "zip.workerScriptsPath =..." and uncomment "zip.workerScripts = ..."
-/*
-workerScripts = {
-	// default zip.js implementation
-	deflate: ["../dist/z-worker.js", "../dist/deflate.js", "../dist/crypto.js"],
-	inflate: ["../dist/z-worker.js", "../dist/inflate.js", "../dist/crypto.js"],
-
-	// pako
-	// deflate: ["../dist/z-worker.js", "../worker-wrappers/pako/pako.min.js", "../pako/codecs.js", "../dist/crypto.js"],
-	// inflate: ["../dist/z-worker.js", "../worker-wrappers/pako/pako.min.js", "../pako/codecs.js", "../dist/crypto.js"],
-};
-*/
