cff859be9f52cad188ab3c62539e438fcdc820cc
Gildas <gildas.lormeau@gmail.com>
2021-01-17 16:12:47 +0100

    remove crypto.js

--- demos/lib/zip.js
+++ demos/lib/zip.js
@@ -435,10 +435,244 @@
 	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	 */
 
+	const ERR_INVALID_PASSORD = "Invalid pasword";
+	const BLOCK_LENGTH = 16;
+	const PBKDF2_ALGORITHM = { name: "PBKDF2" };
+	const SIGNATURE_ALGORITHM = { name: "HMAC" };
+	const HASH_FUNCTION = "SHA-1";
+	const CRYPTO_KEY_ALGORITHM = { name: "AES-CTR" };
+	const BASE_KEY_ALGORITHM = Object.assign({ hash: SIGNATURE_ALGORITHM }, PBKDF2_ALGORITHM);
+	const DERIVED_BITS_ALGORITHM = Object.assign({ iterations: 1000, hash: { name: HASH_FUNCTION } }, PBKDF2_ALGORITHM);
+	const AUTHENTICATION_ALGORITHM = Object.assign({ hash: HASH_FUNCTION }, SIGNATURE_ALGORITHM);
+	const CRYPTO_ALGORITHM = Object.assign({ length: BLOCK_LENGTH }, CRYPTO_KEY_ALGORITHM);
+	const DERIVED_BITS_USAGE = ["deriveBits"];
+	const SIGN_USAGE = ["sign"];
+	const DERIVED_BITS_LENGTH = 528;
+	const PREAMBULE_LENGTH = 18;
+	const SIGNATURE_LENGTH = 10;
+	const COUNTER_DEFAULT_VALUE = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+	const subtle = crypto.subtle;
+
+	class ZipDecrypt {
+
+		constructor(password, signed) {
+			this.password = password;
+			this.signed = signed;
+			this.input = signed && new Uint8Array(0);
+			this.pendingInput = new Uint8Array(0);
+		}
+
+		async append(input) {
+			const decrypt = async (offset = 0) => {
+				if (offset + BLOCK_LENGTH <= buferredInput.length - SIGNATURE_LENGTH) {
+					const chunkToDecrypt = buferredInput.subarray(offset, offset + BLOCK_LENGTH);
+					const outputChunk = await subtle.decrypt(Object.assign({ counter: this.counter }, CRYPTO_ALGORITHM), this.keys.decryption, chunkToDecrypt);
+					incrementCounter(this.counter);
+					output.set(new Uint8Array(outputChunk), offset);
+					return decrypt(offset + BLOCK_LENGTH);
+				} else {
+					this.pendingInput = buferredInput.subarray(offset);
+					if (this.signed) {
+						this.input = concat(this.input, input);
+					}
+					return output;
+				}
+			};
+
+			if (this.password) {
+				const preambule = input.subarray(0, PREAMBULE_LENGTH);
+				await createDecryptionKeys(this, preambule, this.password);
+				this.password = null;
+				input = input.subarray(PREAMBULE_LENGTH);
+			}
+			let output = new Uint8Array(input.length - SIGNATURE_LENGTH - ((input.length - SIGNATURE_LENGTH) % BLOCK_LENGTH));
+			let buferredInput = input;
+			if (this.pendingInput.length) {
+				buferredInput = concat(this.pendingInput, input);
+				output = expand(output, buferredInput.length - SIGNATURE_LENGTH - ((buferredInput.length - SIGNATURE_LENGTH) % BLOCK_LENGTH));
+			}
+			return decrypt();
+		}
+
+		async flush() {
+			const pendingInput = this.pendingInput;
+			const keys = this.keys;
+			const chunkToDecrypt = pendingInput.subarray(0, pendingInput.length - SIGNATURE_LENGTH);
+			const originalSignatureArray = pendingInput.subarray(pendingInput.length - SIGNATURE_LENGTH);
+			let decryptedChunkArray = new Uint8Array(0);
+			if (chunkToDecrypt.length) {
+				const decryptedChunk = await subtle.decrypt(Object.assign({ counter: this.counter }, CRYPTO_ALGORITHM), keys.decryption, chunkToDecrypt);
+				decryptedChunkArray = new Uint8Array(decryptedChunk);
+			}
+			let valid = true;
+			if (this.signed) {
+				const signature = await subtle.sign(SIGNATURE_ALGORITHM, keys.authentication, this.input.subarray(0, this.input.length - SIGNATURE_LENGTH));
+				const signatureArray = new Uint8Array(signature);
+				this.input = null;
+				for (let indexSignature = 0; indexSignature < SIGNATURE_LENGTH; indexSignature++) {
+					if (signatureArray[indexSignature] != originalSignatureArray[indexSignature]) {
+						valid = false;
+					}
+				}
+			}
+			return {
+				valid,
+				data: decryptedChunkArray
+			};
+		}
+
+	}
+
+	class ZipEncrypt {
+
+		constructor(password) {
+			this.password = password;
+			this.output = new Uint8Array(0);
+			this.pendingInput = new Uint8Array(0);
+		}
+
+		async append(input) {
+			const encrypt = async (offset = 0) => {
+				if (offset + BLOCK_LENGTH <= input.length) {
+					const chunkToEncrypt = input.subarray(offset, offset + BLOCK_LENGTH);
+					const outputChunk = await subtle.encrypt(Object.assign({ counter: this.counter }, CRYPTO_ALGORITHM), this.keys.encryption, chunkToEncrypt);
+					incrementCounter(this.counter);
+					output.set(new Uint8Array(outputChunk), offset + preambule.length);
+					return encrypt(offset + BLOCK_LENGTH);
+				} else {
+					this.pendingInput = input.subarray(offset);
+					this.output = concat(this.output, output);
+					return output;
+				}
+			};
+
+			let preambule = new Uint8Array(0);
+			if (this.password) {
+				preambule = await createEncryptionKeys(this, this.password);
+				this.password = null;
+			}
+			let output = new Uint8Array(preambule.length + input.length - (input.length % BLOCK_LENGTH));
+			output.set(preambule, 0);
+			if (this.pendingInput.length) {
+				input = concat(this.pendingInput, input);
+				output = expand(output, input.length - (input.length % BLOCK_LENGTH));
+			}
+			return encrypt();
+		}
+
+		async flush() {
+			let encryptedChunkArray = new Uint8Array(0);
+			if (this.pendingInput.length) {
+				const encryptedChunk = await subtle.encrypt(Object.assign({ counter: this.counter }, CRYPTO_ALGORITHM), this.keys.encryption, this.pendingInput);
+				encryptedChunkArray = new Uint8Array(encryptedChunk);
+				this.output = concat(this.output, encryptedChunkArray);
+			}
+			const signature = await subtle.sign(SIGNATURE_ALGORITHM, this.keys.authentication, this.output.subarray(PREAMBULE_LENGTH));
+			this.output = null;
+			const signatureArray = new Uint8Array(signature).subarray(0, SIGNATURE_LENGTH);
+			return {
+				data: concat(encryptedChunkArray, signatureArray),
+				signature: signatureArray
+			};
+		}
+	}
+
+	async function createDecryptionKeys(decryption, preambuleArray, password) {
+		decryption.counter = new Uint8Array(COUNTER_DEFAULT_VALUE);
+		const salt = preambuleArray.subarray(0, 16);
+		const passwordVerification = preambuleArray.subarray(16);
+		const encodedPassword = (new TextEncoder()).encode(password);
+		const basekey = await subtle.importKey("raw", encodedPassword, BASE_KEY_ALGORITHM, false, DERIVED_BITS_USAGE);
+		const derivedBits = await subtle.deriveBits(Object.assign({ salt }, DERIVED_BITS_ALGORITHM), basekey, DERIVED_BITS_LENGTH);
+		const compositeKey = new Uint8Array(derivedBits);
+		const passwordVerificationKey = compositeKey.subarray(64);
+		decryption.keys = {
+			decryption: await subtle.importKey("raw", compositeKey.subarray(0, 32), CRYPTO_KEY_ALGORITHM, true, ["decrypt"]),
+			authentication: await subtle.importKey("raw", compositeKey.subarray(32, 64), AUTHENTICATION_ALGORITHM, false, SIGN_USAGE),
+			passwordVerification: passwordVerificationKey
+		};
+		if (passwordVerificationKey[0] != passwordVerification[0] || passwordVerificationKey[1] != passwordVerification[1]) {
+			throw new Error(ERR_INVALID_PASSORD);
+		}
+	}
+
+	async function createEncryptionKeys(encryption, password) {
+		encryption.counter = new Uint8Array(COUNTER_DEFAULT_VALUE);
+		const salt = crypto.getRandomValues(new Uint8Array(16));
+		const encodedPassword = (new TextEncoder()).encode(password);
+		const basekey = await subtle.importKey("raw", encodedPassword, BASE_KEY_ALGORITHM, false, DERIVED_BITS_USAGE);
+		const derivedBits = await subtle.deriveBits(Object.assign({ salt }, DERIVED_BITS_ALGORITHM), basekey, DERIVED_BITS_LENGTH);
+		const compositeKey = new Uint8Array(derivedBits);
+		encryption.keys = {
+			encryption: await subtle.importKey("raw", compositeKey.subarray(0, 32), CRYPTO_KEY_ALGORITHM, true, ["encrypt"]),
+			authentication: await subtle.importKey("raw", compositeKey.subarray(32, 64), AUTHENTICATION_ALGORITHM, false, SIGN_USAGE),
+			passwordVerification: compositeKey.subarray(64)
+		};
+		return concat(salt, encryption.keys.passwordVerification);
+	}
+
+	function incrementCounter(counter) {
+		for (let indexCounter = 0; indexCounter < 16; indexCounter++) {
+			if (counter[indexCounter] == 255) {
+				counter[indexCounter] = 0;
+			} else {
+				counter[indexCounter]++;
+				break;
+			}
+		}
+	}
+
+	function concat(leftArray, rightArray) {
+		let array = leftArray;
+		if (leftArray.length + rightArray.length) {
+			array = new Uint8Array(leftArray.length + rightArray.length);
+			array.set(leftArray, 0);
+			array.set(rightArray, leftArray.length);
+		}
+		return array;
+	}
+
+	function expand(inputArray, length) {
+		if (length && length > inputArray.length) {
+			const array = inputArray;
+			inputArray = new Uint8Array(length);
+			inputArray.set(array, 0);
+		}
+		return inputArray;
+	}
+
+	/*
+	 Copyright (c) 2021 Gildas Lormeau. All rights reserved.
+
+	 Redistribution and use in source and binary forms, with or without
+	 modification, are permitted provided that the following conditions are met:
+
+	 1. Redistributions of source code must retain the above copyright notice,
+	 this list of conditions and the following disclaimer.
+
+	 2. Redistributions in binary form must reproduce the above copyright 
+	 notice, this list of conditions and the following disclaimer in 
+	 the documentation and/or other materials provided with the distribution.
+
+	 3. The names of the authors may not be used to endorse or promote products
+	 derived from this software without specific prior written permission.
+
+	 THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+	 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+	 FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JCRAFT,
+	 INC. OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
+	 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	 LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+	 OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	 LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	 */
+
 	const Z_WORKER_SCRIPT_PATH = "z-worker.js";
 	const DEFAULT_WORKER_SCRIPTS = {
-		deflate: [Z_WORKER_SCRIPT_PATH, "deflate.js", "crypto.js"],
-		inflate: [Z_WORKER_SCRIPT_PATH, "inflate.js", "crypto.js"]
+		deflate: [Z_WORKER_SCRIPT_PATH, "deflate.js"],
+		inflate: [Z_WORKER_SCRIPT_PATH, "inflate.js"]
 	};
 	const workers = {
 		pool: [],
@@ -689,6 +923,14 @@
 		}
 	}
 
+	async function createBaseCodec(options) {
+		if (options.codecType.startsWith("deflate")) {
+			return new Deflate(options);
+		} else if (options.codecType.startsWith("inflate")) {
+			return new Inflate(options);
+		}
+	}
+
 	async function createCodec(config, options) {
 		const streamCopy =
 			!options.inputCompressed && !options.inputSigned && !options.inputEncrypted &&
@@ -696,11 +938,7 @@
 		if (config.useWebWorkers && !streamCopy) {
 			return createWorkerCodec(config, options);
 		} else {
-			if (options.codecType.startsWith("deflate")) {
-				return new Deflate(options);
-			} else if (options.codecType.startsWith("inflate")) {
-				return new Inflate(options);
-			}
+			return createBaseCodec(options);
 		}
 	}
 
@@ -865,7 +1103,7 @@
 				entry.offset = directoryDataView.getUint32(offset + 42, true);
 				entry.rawFilename = dataArray.subarray(offset + 46, offset + 46 + entry.filenameLength);
 				const filename = getString(entry.rawFilename);
-				entry.filename = ((entry.bitFlag & 0x0800) == 0x0800) ? decodeUTF8(filename) : decodeASCII(filename);
+				entry.filename = entry.bitFlag.languageEncodingFlag ? decodeUTF8(filename) : decodeASCII(filename);
 				if (!entry.directory && entry.filename.charAt(entry.filename.length - 1) == "/") {
 					entry.directory = true;
 				}
@@ -874,7 +1112,7 @@
 				entry.rawComment = dataArray.subarray(offset + 46 + entry.filenameLength + entry.extraFieldLength, offset + 46
 					+ entry.filenameLength + entry.extraFieldLength + entry.commentLength);
 				const comment = getString(entry.rawComment);
-				entry.comment = ((entry.bitFlag & 0x0800) == 0x0800) ? decodeUTF8(comment) : decodeASCII(comment);
+				entry.comment = entry.bitFlag.languageEncodingFlag ? decodeUTF8(comment) : decodeASCII(comment);
 				entries.push(entry);
 				offset += 46 + entry.filenameLength + entry.extraFieldLength + entry.commentLength;
 			}
@@ -925,7 +1163,13 @@
 
 	function readCommonHeader(entry, dataView, offset) {
 		entry.version = dataView.getUint16(offset, true);
-		entry.bitFlag = dataView.getUint16(offset + 2, true);
+		const rawBitFlag = entry.rawBitFlag = dataView.getUint16(offset + 2, true);
+		entry.bitFlag = {
+			encrypted: (rawBitFlag & 0x01) == 0x01,
+			level: (rawBitFlag & 0x06) >> 1,
+			dataDescriptor: (rawBitFlag & 0x08) == 0x08,
+			languageEncodingFlag: (rawBitFlag & 0x0800) == 0x0800
+		};
 		entry.filenameLength = dataView.getUint16(offset + 22, true);
 		entry.extraFieldLength = dataView.getUint16(offset + 24, true);
 	}
@@ -956,21 +1200,19 @@
 					extraFieldZip64.values.push(Number(extraFieldView.getBigUint64(0 + indexValue * 8, true)));
 				}
 			}
-			if (extraFieldAES) {
-				if ((entry.bitFlag & 0x01) == 0x01) {
-					entry.passwordProtected = true;
-					const compressionMethod = dataView.getUint16(offset + 4, true);
-					if (compressionMethod != 0x63) {
-						throw new Error(ERR_UNSUPPORTED_COMPRESSION);
-					}
-					const extraFieldView = new DataView(extraFieldAES.data.buffer);
-					extraFieldAES.vendorVersion = extraFieldView.getUint8(0);
-					extraFieldAES.vendorId = extraFieldView.getUint8(2);
-					const strength = extraFieldView.getUint8(4);
-					extraFieldAES.compressionMethod = extraFieldView.getUint16(5, true);
-					if (strength != 3) {
-						throw new Error(ERR_UNSUPPORTED_ENCRYPTION);
-					}
+			if (extraFieldAES && entry.bitFlag.encrypted) {
+				entry.passwordProtected = true;
+				const compressionMethod = dataView.getUint16(offset + 4, true);
+				if (compressionMethod != 0x63) {
+					throw new Error(ERR_UNSUPPORTED_COMPRESSION);
+				}
+				const extraFieldView = new DataView(extraFieldAES.data.buffer);
+				extraFieldAES.vendorVersion = extraFieldView.getUint8(0);
+				extraFieldAES.vendorId = extraFieldView.getUint8(2);
+				const strength = extraFieldView.getUint8(4);
+				extraFieldAES.compressionMethod = extraFieldView.getUint16(5, true);
+				if (strength != 3) {
+					throw new Error(ERR_UNSUPPORTED_ENCRYPTION);
 				}
 			}
 		}
@@ -982,9 +1224,12 @@
 		if (entry.compressionMethod != 0x0 && entry.compressionMethod != 0x08) {
 			throw new Error(ERR_UNSUPPORTED_COMPRESSION);
 		}
+		if (entry.compressionMethod == 0x08) {
+			entry.bitFlag.enhancedDeflating = (entry.rawBitFlag & 0x10) != 0x10;
+		}
 		entry.lastModDateRaw = dataView.getUint32(offset + 6, true);
 		entry.lastModDate = getDate(entry.lastModDateRaw);
-		if (isCentralHeader || (entry.bitFlag & 0x08) != 0x08) {
+		if (isCentralHeader || !entry.bitFlag.dataDescriptor) {
 			entry.signature = dataView.getUint32(offset + 10, true);
 			entry.uncompressedSize = dataView.getUint32(offset + 18, true);
 			entry.compressedSize = dataView.getUint32(offset + 14, true);
