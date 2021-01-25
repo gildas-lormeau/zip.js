d1ececa735432a05dd90a579750d58060a446a63
Gildas <gildas.lormeau@gmail.com>
2021-01-25 14:47:04 +0100

    add script to build inline web worker

---
+++ rollup-inline-worker.config.js
@@ -0,0 +1,32 @@
+import { terser } from "rollup-plugin-terser";
+
+export default [
+	{
+		input: "lib/z-worker.js",
+		output: [
+			{
+				file: "lib/z-worker-inline.js",
+				format: "es",
+				name: "worker",
+				plugins: [
+					terser()
+				]
+			}
+		]
+	},
+	{
+		input: "lib/z-worker-inline.js",
+		output: [
+			{
+				file: "lib/z-worker-inline.js",
+				format: "esm",
+				name: "worker",
+				intro: "import { configure } from \"./core/zip-core.js\"; function configureWebWorker() { const uri = URL.createObjectURL(new Blob([`",
+				outro: "`])); configure({ workerScriptsPath: null, workerScripts: { inflate: [uri], deflate: [uri] } }); } export default configureWebWorker;",
+				plugins: [
+					terser()
+				]
+			}
+		]
+	}
+];
