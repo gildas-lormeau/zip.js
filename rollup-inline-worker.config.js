49f4ed1f4baf580da1766afd18d642d412953809
Gildas <gildas.lormeau@gmail.com>
2021-01-27 00:50:24 +0100

    format code

--- rollup-inline-worker.config.js
+++ rollup-inline-worker.config.js
@@ -1,32 +1,19 @@
 import { terser } from "rollup-plugin-terser";
 
-export default [
-	{
-		input: "lib/z-worker.js",
-		output: [
-			{
-				file: "lib/z-worker-inline.js",
-				format: "es",
-				name: "worker",
-				plugins: [
-					terser()
-				]
-			}
-		]
-	},
-	{
-		input: "lib/z-worker-inline.js",
-		output: [
-			{
-				file: "lib/z-worker-inline.js",
-				format: "esm",
-				name: "worker",
-				intro: "import { configure } from './core/zip-core.js'; export default () => { const uri = URL.createObjectURL(new Blob(['(' + (() => {",
-				outro: "}).toString() + ')()'])); configure({ workerScripts: { inflate: [uri], deflate: [uri] } }); };",
-				plugins: [
-					terser()
-				]
-			}
-		]
-	}
-];
+export default [{
+	input: "lib/z-worker.js",
+	output: [{
+		file: "lib/z-worker-inline.js",
+		format: "es",
+		plugins: [terser()]
+	}]
+}, {
+	input: "lib/z-worker-inline.js",
+	output: [{
+		file: "lib/z-worker-inline.js",
+		format: "esm",
+		intro: "import { configure } from './core/zip-core.js'; export default () => { const uri = URL.createObjectURL(new Blob(['(' + (() => {",
+		outro: "}).toString() + ')()'])); configure({ workerScripts: { inflate: [uri], deflate: [uri] } }); };",
+		plugins: [terser()]
+	}]
+}];
--- rollup.config.js
+++ rollup.config.js
@@ -1,104 +1,69 @@
 import { terser } from "rollup-plugin-terser";
 
-export default [
-	{
-		input: ["lib/zip.js"],
-		output: [
-			{
-				file: "dist/zip.min.js",
-				format: "umd",
-				name: "zip",
-				plugins: [
-					terser()
-				]
-			},
-			{
-				file: "dist/zip.js",
-				format: "umd",
-				name: "zip",
-				plugins: [
-				]
-			}
-		]
-	},
-	{
-		input: ["lib/zip-full.js"],
-		output: [
-			{
-				file: "dist/zip-full.min.js",
-				format: "umd",
-				name: "zip",
-				plugins: [
-					terser()
-				]
-			},
-			{
-				file: "dist/zip-full.js",
-				format: "umd",
-				name: "zip",
-				plugins: [
-				]
-			}
-		]
+export default [{
+	input: ["lib/zip.js"],
+	output: [{
+		file: "dist/zip.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: [terser()]
 	}, {
-		input: "lib/zip-fs.js",
-		output: [
-			{
-				file: "dist/zip-fs.min.js",
-				format: "umd",
-				name: "zip",
-				plugins: [
-					terser()
-				]
-			},
-			{
-				file: "dist/zip-fs.js",
-				format: "umd",
-				name: "zip",
-				plugins: [
-				]
-			}
-		]
+		file: "dist/zip.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: ["lib/zip-full.js"],
+	output: [{
+		file: "dist/zip-full.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: [terser()]
 	}, {
-		input: "index.js",
-		output: [
-			{
-				file: "dist/zip-fs-full.js",
-				format: "umd",
-				name: "zip",
-				plugins: [
-				]
-			},
-			{
-				file: "dist/zip-fs-full.min.js",
-				format: "umd",
-				name: "zip",
-				plugins: [
-					terser()
-				]
-			}
-		]
+		file: "dist/zip-full.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: "lib/zip-fs.js",
+	output: [{
+		file: "dist/zip-fs.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: [terser()]
 	}, {
-		input: "lib/z-worker-pako.js",
-		output: [
-			{
-				file: "dist/z-worker-pako.js",
-				format: "iife",
-				plugins: [
-					terser()
-				]
-			}
-		]
+		file: "dist/zip-fs.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: "index.js",
+	output: [{
+		file: "dist/zip-fs-full.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
 	}, {
-		input: "lib/z-worker.js",
-		output: [
-			{
-				file: "dist/z-worker.js",
-				format: "iife",
-				plugins: [
-					terser()
-				]
-			}
-		]
-	}
-];
+		file: "dist/zip-fs-full.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: [terser()]
+	}]
+}, {
+	input: "lib/z-worker-pako.js",
+	output: [{
+		file: "dist/z-worker-pako.js",
+		format: "iife",
+		plugins: [terser()]
+	}]
+}, {
+	input: "lib/z-worker.js",
+	output: [{
+		file: "dist/z-worker.js",
+		format: "iife",
+		plugins: [terser()]
+	}]
+}];
