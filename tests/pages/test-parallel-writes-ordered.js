e21c1edb36ac5d338992aabe6c8b305156e98dcf
Gildas <gildas.lormeau@gmail.com>
2021-02-07 01:14:21 +0100

    improve test

--- tests/pages/test-parallel-writes-ordered.js
+++ tests/pages/test-parallel-writes-ordered.js
@@ -5,7 +5,7 @@
 
 const KB = 1024;
 const ENTRIES_DATA = [
-	{ name: "entry #1", blob: getBlob(8.5 * KB) }, { name: "entry #2", blob: getBlob(50.2 * KB) }, { name: "entry #3", blob: getBlob(4.7 * KB) },
+	{ name: "entry #1", blob: getBlob(8.5 * KB) }, { name: "entry #2", blob: getBlob(10.2 * KB) }, { name: "entry #3", blob: getBlob(64.7 * KB) },
 	{ name: "entry #4", blob: getBlob(2.8 * KB) }, { name: "entry #5", blob: getBlob(30.9 * KB) }, { name: "entry #6", blob: getBlob(76.2 * KB) },
 	{ name: "entry #7", blob: getBlob(5.1 * KB) }, { name: "entry #8", blob: getBlob(42.6 * KB) }, { name: "entry #9", blob: getBlob(3.1 * KB) }];
 
@@ -22,7 +22,7 @@ async function test() {
 	await zipWriter.close();
 	const zipReader = new zip.ZipReader(new zip.BlobReader(blobWriter.getData()));
 	const entries = await zipReader.getEntries();
-	if (JSON.stringify(entries.sort((entry1, entry2) => entry1.filename.localeCompare(entry2.filename)).map(entry => entry.filename)) ==
+	if (JSON.stringify(ENTRIES_DATA.map(entry => entry.name)) ==
 		JSON.stringify(entries.sort((entry1, entry2) => entry1.offset - entry2.offset).map(entry => entry.filename))) {
 		document.body.innerHTML = "ok";
 	}
