20f864463d3d728f0aa116b43507257157de42e7
Gildas <gildas.lormeau@gmail.com>
2021-02-14 21:11:42 +0100

    add support of ZipCrypto

---
+++ tests/pages/test-zipcrypto.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-console */
+/* global zip, document, Blob, FileReader */
+
+"use strict";
+
+const TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
+const FILENAME = "lorem.txt";
+const BLOB = new Blob([TEXT_CONTENT], { type: zip.getMimeType(FILENAME) });
+
+test().catch(error => console.error(error));
+
+async function test() {
+	document.body.innerHTML = "...";
+	zip.configure({ chunkSize: 128 });
+	const blobWriter = new zip.BlobWriter("application/zip");
+	const zipWriter = new zip.ZipWriter(blobWriter);
+	await zipWriter.add(FILENAME, new zip.BlobReader(BLOB), { password: "password", zipCrypto: true });
+	await zipWriter.close();
+	const zipReader = new zip.ZipReader(new zip.BlobReader(blobWriter.getData()));
+	const entries = await zipReader.getEntries();
+	const dataBlobWriter = new zip.BlobWriter(zip.getMimeType(entries[0].filename));
+	let data;
+	if (entries[0].encrypted) {
+		try {
+			data = await entries[0].getData(dataBlobWriter, { password: "notagoodpassword" });
+			data = null;
+		} catch (error) {
+			if (error.message == zip.ERR_INVALID_PASSWORD) {
+				data = await entries[0].getData(dataBlobWriter, { password: "password" });
+			}
+		}
+	}
+	await zipReader.close();
+	if (TEXT_CONTENT == (await getBlobText(data)) && entries[0].filename == FILENAME && entries[0].uncompressedSize == TEXT_CONTENT.length) {
+		document.body.innerHTML = "ok";
+	}
+}
+
+// eslint-disable-next-line no-unused-vars
+async function logBlobText(blob) {
+	console.log(await getBlobText(blob));
+	console.log("--------------");
+}
+
+async function getBlobText(blob) {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.onload = event => resolve(event.target.result);
+		reader.onerror = reject;
+		reader.readAsText(blob);
+	});
+}
