e12a2214d79d186498a1d9b74740d59b03166e37
Gildas <gildas.lormeau@gmail.com>
2021-03-03 23:18:19 +0100

    add dev file

---
+++ rollup.config.dev.js
@@ -0,0 +1,98 @@
+export default [{
+	input: ["lib/zip.js"],
+	output: [{
+		file: "dist/zip.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}, {
+		file: "dist/zip.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: ["lib/zip-full.js"],
+	output: [{
+		file: "dist/zip-full.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}, {
+		file: "dist/zip-full.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: "lib/zip-no-worker.js",
+	output: [{
+		file: "dist/zip-no-worker.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: "lib/zip-no-worker-deflate.js",
+	output: [{
+		file: "dist/zip-no-worker-deflate.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: "lib/zip-no-worker-inflate.js",
+	output: [{
+		file: "dist/zip-no-worker-inflate.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: "lib/zip-fs.js",
+	output: [{
+		file: "dist/zip-fs.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}, {
+		file: "dist/zip-fs.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: "index.js",
+	output: [{
+		file: "dist/zip-fs-full.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}, {
+		file: "dist/zip-fs-full.min.js",
+		format: "umd",
+		name: "zip",
+		plugins: []
+	}]
+}, {
+	input: "lib/z-worker-pako.js",
+	output: [{
+		file: "dist/z-worker-pako.js",
+		format: "iife",
+		plugins: []
+	}]
+}, {
+	input: "lib/z-worker-fflate.js",
+	output: [{
+		file: "dist/z-worker-fflate.js",
+		format: "iife",
+		plugins: []
+	}]
+}, {
+	input: "lib/z-worker.js",
+	output: [{
+		file: "dist/z-worker.js",
+		format: "iife",
+		plugins: []
+	}]
+}];
