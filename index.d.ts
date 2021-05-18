76e78644e9b07164ec0e5337f790a2f4ddb2685f
Gildas <gildas.lormeau@gmail.com>
2021-05-19 01:12:03 +0200

    throw error if detecting zip64 need and keepOrder is false

--- index.d.ts
+++ index.d.ts
@@ -251,6 +251,7 @@ declare module "@zip.js/zip.js" {
     export const ERR_INVALID_EXTRAFIELD_TYPE: string;
     export const ERR_INVALID_EXTRAFIELD_DATA: string;
     export const ERR_INVALID_ENCRYPTION_STRENGTH: string;
-    export const ERR_ABORT: string;
+    export const ERR_NOT_SUPPORTED_FORMAT: string;
+    export const ERR_ABORT: string;    
 
 }
