f8da5fec17bdef932cee9f0bf034d2407879ddb8
Gildas <gildas.lormeau@gmail.com>
2021-11-12 02:05:12 +0100

    update dependencies

--- dist/zip-fs-full.js
+++ dist/zip-fs-full.js
@@ -2,7 +2,7 @@
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
 	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.zip = {}));
-}(this, (function (exports) { 'use strict';
+})(this, (function (exports) { 'use strict';
 
 	/*
 	 Copyright (c) 2021 Gildas Lormeau. All rights reserved.
@@ -9481,4 +9481,4 @@
 
 	Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));
