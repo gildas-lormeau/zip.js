7e88f4e0ccc06c9199a68fc0584e406280a9a9d6
Gildas <gildas.lormeau@gmail.com>
2021-11-12 03:20:19 +0100

    upadte code

--- tests/node/test-node.js
+++ tests/node/test-node.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-/* global globalThis */
 
 "use strict";
 
@@ -8,7 +7,7 @@ import { Blob } from "blob-polyfill";
 globalThis.Blob = Blob;
 globalThis.Worker = Worker;
 
-const { BlobWriter, BlobReader, ZipWriter, ZipReader, terminateWorkers } = await import("../../index.js");
+const { BlobWriter, BlobReader, ZipWriter, ZipReader, terminateWorkers } = await import("../../index.js"); // the import is async to make sure globalThis.Blob is defined
 
 const TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
 const FILENAME = "lorem.txt";
