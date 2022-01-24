84da8d5eeea2af348260d1b0220744fede7da7e5
Gildas <gildas.lormeau@gmail.com>
2022-01-24 21:54:14 +0100

    reference import.meta.url only when using web workers (see https://github.com/gildas-lormeau/zip.js/discussions/299)

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ import { configure } from "./lib/core/configuration.js";
 import getMimeType from "./lib/core/util/mime-type.js";
 import { terminateWorkers } from "./lib/core/codecs/codec-pool.js";
 
-configure({ Deflate, Inflate });
+configure({ Deflate, Inflate, baseURL: import.meta.url });
 
 export {
 	fs,
