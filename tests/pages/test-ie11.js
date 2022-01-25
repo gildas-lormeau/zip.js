ce2b34798500fe1073ff39755b9164684eeaac3a
Gildas <gildas.lormeau@gmail.com>
2022-01-25 04:02:05 +0100

    remove unused function

--- tests/pages/test-ie11.js
+++ tests/pages/test-ie11.js
@@ -34,12 +34,3 @@ function test() {
 		}
 	});
 }
-
-function getBlobText(blob) {
-	return new Promise(function (resolve, reject) {
-		const reader = new FileReader();
-		reader.onload = function (event) { resolve(event.target.result); };
-		reader.onerror = function () { reject(reader.error); };
-		reader.readAsText(blob);
-	});
-}
