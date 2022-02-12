1ff27b569b1518060a8391c28b71ffd21466e2ad
Gildas <gildas.lormeau@gmail.com>
2022-02-12 02:15:27 +0100

    remove top-level await

--- tests/node/test-node.js
+++ tests/node/test-node.js
@@ -1,3 +1,4 @@
+/* global globalThis */
 /* eslint-disable no-console */
 
 import Worker from "web-worker";
@@ -5,22 +6,21 @@ import { Blob } from "blob-polyfill";
 globalThis.Blob = Blob;
 globalThis.Worker = Worker;
 
-const { BlobWriter, BlobReader, ZipWriter, ZipReader, terminateWorkers } = await import("../../index.js"); // the import is async to make sure globalThis.Blob is defined
-
 const TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
 const FILENAME = "lorem.txt";
 const TEXT_PLAIN_MIMETYPE = "text/plain";
 const BLOB = new Blob([TEXT_CONTENT], { type: TEXT_PLAIN_MIMETYPE });
 
-test().then(function (result) {
-	if (result) {
-		console.error("ok");
-	}
-}).catch(function (error) {
-	console.error(error);
-});
+// the import is async to make sure globalThis.Blob is defined
+import("../../index.js")
+	.then(zip => test(zip))
+	.then(result => {
+		if (result) {
+			console.error("ok");
+		}
+	}).catch(error => console.error(error));
 
-async function test() {
+async function test({ BlobWriter, BlobReader, ZipWriter, ZipReader, terminateWorkers }) {
 	const blobWriter = new BlobWriter("application/zip");
 	const zipWriter = new ZipWriter(blobWriter);
 	const entry = await zipWriter.add(FILENAME, new BlobReader(BLOB));
