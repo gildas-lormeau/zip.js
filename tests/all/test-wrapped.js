fe9a3b1e05775477c1652f4c7cd58330fb485a63
Gildas <gildas.lormeau@gmail.com>
2022-07-16 00:19:24 +0200

    extract comment and extra data

--- tests/all/test-wrapped.js
+++ tests/all/test-wrapped.js
@@ -5,20 +5,24 @@ import * as zip from "../../index.js";
 const TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
 const FILENAME = "lorem.txt";
 const BLOB = new Blob([TEXT_CONTENT], { type: zip.getMimeType(FILENAME) });
+const COMMENT = new Uint8Array([60, 61, 62]);
+const PREPENDED_DATA = new Uint8Array([60, 61, 62]);
 
 export { test };
 
 async function test() {
 	zip.configure({ chunkSize: 128, useWebWorkers: true });
 	const blobWriter = new zip.BlobWriter("application/zip");
-	await blobWriter.writeUint8Array(new Uint8Array([60, 61, 62]));
+	await blobWriter.writeUint8Array(PREPENDED_DATA);
 	const zipWriter = new zip.ZipWriter(blobWriter);
 	await zipWriter.add(FILENAME, new zip.BlobReader(BLOB));
-	await zipWriter.close(new Uint8Array([60, 61, 62]));
-	const zipReader = new zip.ZipReader(new zip.BlobReader(blobWriter.getData()));
+	await zipWriter.close(COMMENT);
+	const zipReader = new zip.ZipReader(new zip.BlobReader(blobWriter.getData()), { extractPrependedData: true });
 	const entries = await zipReader.getEntries();
 	const data = await entries[0].getData(new zip.BlobWriter(zip.getMimeType(entries[0].filename)));
 	await zipReader.close();
+	const comment = Array.from(zipReader.comment).toString();
+	const prependedData = Array.from(zipReader.prependedData).toString();
 	zip.terminateWorkers();
-	return TEXT_CONTENT == (await data.text()) && entries[0].filename == FILENAME && entries[0].uncompressedSize == TEXT_CONTENT.length;
+	return TEXT_CONTENT == (await data.text()) && prependedData == Array.from(PREPENDED_DATA) && comment == Array.from(COMMENT);
 }
