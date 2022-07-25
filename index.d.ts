92567bb38749371c753d583090e2b20434957e13
Gildas <gildas.lormeau@gmail.com>
2022-07-25 02:36:59 +0200

    fix implements

--- index.d.ts
+++ index.d.ts
@@ -59,7 +59,7 @@ interface ReadableReader {
     readable: ReadableStream<any>;
 }
 
-export class Reader<Type> implements RandomAccessReader, ReadableReader {
+export class Reader<Type> implements ReadableReader {
     constructor(value: Type);
     readUint8Array(index: number, length: number): Promise<Uint8Array>;
 }
