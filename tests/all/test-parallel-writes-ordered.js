9c89a42c46a9aa89686623f2b8e32a75fe147041
Gildas <gildas.lormeau@gmail.com>
2022-07-29 17:37:08 +0200

    improve test

--- tests/all/test-parallel-writes-ordered.js
+++ tests/all/test-parallel-writes-ordered.js
@@ -4,14 +4,14 @@ import * as zip from "../../index.js";
 
 const KB = 1024;
 const ENTRIES_DATA = [
-	{ name: "entry #1", blob: getBlob(8.5 * KB) }, { name: "entry #2", blob: getBlob(10.2 * KB) }, { name: "entry #3", blob: getBlob(64.7 * KB) },
-	{ name: "entry #4", blob: getBlob(2.8 * KB) }, { name: "entry #5", blob: getBlob(30.9 * KB) }, { name: "entry #6", blob: getBlob(76.2 * KB) },
-	{ name: "entry #7", blob: getBlob(5.1 * KB) }, { name: "entry #8", blob: getBlob(42.6 * KB) }, { name: "entry #9", blob: getBlob(3.1 * KB) }];
+	{ name: "entry #1", blob: getBlob(8.5 * KB) }, { name: "entry #2", blob: getBlob(100.2 * KB) }, { name: "entry #3", blob: getBlob(640.7 * KB) },
+	{ name: "entry #4", blob: getBlob(200.8 * KB) }, { name: "entry #5", blob: getBlob(3.9 * KB) }, { name: "entry #6", blob: getBlob(7.2 * KB) },
+	{ name: "entry #7", blob: getBlob(5.1 * KB) }, { name: "entry #8", blob: getBlob(4.6 * KB) }, { name: "entry #9", blob: getBlob(3.1 * KB) }];
 
 export { test };
 
 async function test() {
-	zip.configure({ chunkSize: 512, useWebWorkers: true });
+	zip.configure({ chunkSize: 512, useWebWorkers: true, maxWorkers: 4 });
 	const blobWriter = new zip.BlobWriter("application/zip");
 	const zipWriter = new zip.ZipWriter(blobWriter, { keepOrder: true });
 	await Promise.all(ENTRIES_DATA.map(entryData => zipWriter.add(entryData.name, new zip.BlobReader(entryData.blob))));
