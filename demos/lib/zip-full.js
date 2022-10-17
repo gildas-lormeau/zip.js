433bcea9526f27414e0f6e1f033ea1c060a76d18
Gildas <gildas.lormeau@gmail.com>
2022-10-17 03:04:09 +0200

    update lib

--- demos/lib/zip-full.js
+++ demos/lib/zip-full.js
@@ -6923,7 +6923,6 @@
 
 	async function readUint8ArrayHttpReader(httpReader, index, length, sendRequest, getRequestData) {
 		const {
-			data,
 			useRangeHeader,
 			forceRangeRequests,
 			options
@@ -6935,10 +6934,11 @@
 			}
 			return new Uint8Array(await response.arrayBuffer());
 		} else {
+			const { data } = httpReader;
 			if (!data) {
 				await getRequestData(httpReader, options);
 			}
-			return new Uint8Array(data.subarray(index, index + length));
+			return new Uint8Array(httpReader.data.subarray(index, index + length));
 		}
 	}
 
