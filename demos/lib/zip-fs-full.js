575e198b149dee220c3e41ea51670aeb1d2c7490
Gildas <gildas.lormeau@gmail.com>
2023-02-20 01:47:44 +0100

    update lib

--- demos/lib/zip-fs-full.js
+++ demos/lib/zip-fs-full.js
@@ -6494,8 +6494,6 @@
 	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	 */
 
-	/* global crypto */
-
 	const GET_RANDOM_VALUES_SUPPORTED = typeof crypto != "undefined" && typeof crypto.getRandomValues == "function";
 
 	const ERR_INVALID_PASSWORD = "Invalid password";
@@ -7623,9 +7621,6 @@
 	 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	 */
-
-	/* global Blob, atob, btoa, XMLHttpRequest, URL, fetch, ReadableStream, WritableStream, FileReader, TransformStream, Response */
-	// deno-lint-ignore-file no-this-alias
 
 	const ERR_HTTP_STATUS = "HTTP error ";
 	const ERR_HTTP_RANGE = "HTTP Range not supported";
@@ -9361,7 +9356,7 @@
 				await skipDiskIfNeeded(writable);
 				fileEntry.diskNumberStart = writer.diskNumber;
 				diskOffset = writer.diskOffset;
-				await blob.stream().pipeTo(writable, { preventClose: true, signal });
+				await blob.stream().pipeTo(writable, { preventClose: true, preventAbort: true, signal });
 				writable.size += blob.size;
 				writingBufferedEntryData = false;
 			}
@@ -9377,7 +9372,11 @@
 			if ((bufferedWrite && writingBufferedEntryData) || (!bufferedWrite && writingEntryData)) {
 				zipWriter.hasCorruptedEntries = true;
 				if (error) {
-					error.corruptedEntry = true;
+					try {
+						error.corruptedEntry = true;
+					} catch (_error) {
+						// ignored
+					}
 				}
 				if (bufferedWrite) {
 					zipWriter.offset += fileWriter.writable.size;
