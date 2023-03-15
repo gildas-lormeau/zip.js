897282471593fe917f07323d16ea4078c332b0f0
Gildas <gildas.lormeau@gmail.com>
2023-03-15 13:23:09 +0100

    add test with a directory in the zip file

---
+++ tests/all/test-directory.js
@@ -0,0 +1,35 @@
+/* global Blob */
+
+import * as zip from "../../index.js";
+
+const TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
+const FOLDER_NAME = "hello/";
+const FILENAME = FOLDER_NAME + "lorem.txt";
+const BLOB = new Blob([TEXT_CONTENT], { type: zip.getMimeType(FILENAME) });
+
+export { test };
+
+async function test() {
+	zip.configure({ chunkSize: 128, useWebWorkers: true });
+	const blobWriter = new zip.BlobWriter("application/zip");
+	const zipWriter = new zip.ZipWriter(blobWriter);
+	await zipWriter.add(FOLDER_NAME, null, { directory: true });
+	await zipWriter.add(FILENAME, new zip.BlobReader(BLOB));
+	await zipWriter.close();
+	const zipReader = new zip.ZipReader(new zip.BlobReader(await blobWriter.getData()));
+	const entries = await zipReader.getEntries();
+	if (entries[0].directory && entries[0].filename == FOLDER_NAME) {
+		if (!entries[1].directory && entries[1].filename == FILENAME) {
+			const text = await entries[1].getData(new zip.TextWriter());
+			await zipReader.close();
+			zip.terminateWorkers();
+			if (TEXT_CONTENT != text) {
+				throw new Error();
+			}
+		} else {
+			throw new Error();
+		}
+	} else {
+		throw new Error();
+	}
+}
