150f36aa62912462fb26fd58ed78abfc7ccb5b0b
Gildas <gildas.lormeau@gmail.com>
2023-04-20 01:24:47 +0200

    mock `fetch`

--- tests/bun-runner.test.js
+++ tests/bun-runner.test.js
@@ -1,6 +1,17 @@
-import { test } from "bun:test";
+/* global globalThis, Bun */
+
+import { test, beforeEach } from "bun:test";
 
 import tests from "./tests-data.js";
+
+beforeEach(() => globalThis.fetch = async (url) => {
+	const file = await Bun.file("." + url.toString().match(/(\/data\/.*)/)[1]);
+	return {
+		status: 200,
+		body: file.stream(),
+		arrayBuffer: () => file.arrayBuffer()
+	};
+});
 
 for (const testData of tests) {
 	if (!testData.env || testData.env.includes("bun")) {
--- tests/node-runner.js
+++ tests/node-runner.js
@@ -1,6 +1,21 @@
-import test from "node:test";
+/* global globalThis */
+
+import { test, mock, beforeEach } from "node:test";
+import { setMaxListeners } from "node:events";
+import { openAsBlob } from "node:fs";
 
 import tests from "./tests-data.js";
+
+setMaxListeners(100);
+
+beforeEach(() => globalThis.fetch = mock.fn(async (url) => {
+	const blob = await openAsBlob("." + url.toString().match(/(\/data\/.*)/)[1]);
+	return {
+		status: 200,
+		body: blob.stream(),
+		arrayBuffer: () => blob.arrayBuffer()
+	};
+}));
 
 for (const testData of tests) {
 	if (!testData.env || testData.env.includes("node")) {
