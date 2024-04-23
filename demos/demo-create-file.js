d91c23e93fd500a7e9a70acc8ee3cadd056694f9
Gildas <gildas.lormeau@gmail.com>
2024-04-23 18:54:15 +0200

    use CompressionStream API when available

--- demos/demo-create-file.js
+++ demos/demo-create-file.js
@@ -14,7 +14,7 @@
 		return {
 			addFile(file, options) {
 				if (!zipWriter) {
-					zipWriter = new zip.ZipWriter(new zip.BlobWriter("application/zip"), { bufferedWrite: true, useCompressionStream: false });
+					zipWriter = new zip.ZipWriter(new zip.BlobWriter("application/zip"), { bufferedWrite: true });
 				}
 				return zipWriter.add(file.name, new zip.BlobReader(file), options);
 			},
