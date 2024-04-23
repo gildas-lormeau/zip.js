c3c55d5cb91ffe3b04b0eaa600ab0949dcb23416
Gildas <gildas.lormeau@gmail.com>
2024-04-23 18:52:09 +0200

    update zip.js

--- demos/lib/zip-fs.js
+++ demos/lib/zip-fs.js
@@ -2779,35 +2779,43 @@
 		const {
 			preventHeadRequest,
 			useRangeHeader,
-			forceRangeRequests
+			forceRangeRequests,
+			combineSizeEocd
 		} = options;
 		options = Object.assign({}, options);
 		delete options.preventHeadRequest;
 		delete options.useRangeHeader;
 		delete options.forceRangeRequests;
+		delete options.combineSizeEocd;
 		delete options.useXHR;
 		Object.assign(httpReader, {
 			url,
 			options,
 			preventHeadRequest,
 			useRangeHeader,
-			forceRangeRequests
+			forceRangeRequests,
+			combineSizeEocd
 		});
 	}
 
 	async function initHttpReader(httpReader, sendRequest, getRequestData) {
 		const {
 			url,
+			preventHeadRequest,
 			useRangeHeader,
-			forceRangeRequests
+			forceRangeRequests,
+			combineSizeEocd
 		} = httpReader;
-		if (isHttpFamily(url) && (useRangeHeader || forceRangeRequests)) {
-			const { headers } = await sendRequest(HTTP_METHOD_GET, httpReader, getRangeHeaders(httpReader));
-			if (!forceRangeRequests && headers.get(HTTP_HEADER_ACCEPT_RANGES) != HTTP_RANGE_UNIT) {
+		if (isHttpFamily(url) && (useRangeHeader || forceRangeRequests) && (typeof preventHeadRequest == "undefined" || preventHeadRequest)) {
+			const response = await sendRequest(HTTP_METHOD_GET, httpReader, getRangeHeaders(httpReader, combineSizeEocd ? -END_OF_CENTRAL_DIR_LENGTH : undefined));
+			if (!forceRangeRequests && response.headers.get(HTTP_HEADER_ACCEPT_RANGES) != HTTP_RANGE_UNIT) {
 				throw new Error(ERR_HTTP_RANGE);
 			} else {
+				if (combineSizeEocd) {
+					httpReader.eocdCache = new Uint8Array(await response.arrayBuffer());
+				}
 				let contentSize;
-				const contentRangeHeader = headers.get(HTTP_HEADER_CONTENT_RANGE);
+				const contentRangeHeader = response.headers.get(HTTP_HEADER_CONTENT_RANGE);
 				if (contentRangeHeader) {
 					const splitHeader = contentRangeHeader.trim().split(/\s*\/\s*/);
 					if (splitHeader.length) {
@@ -2832,9 +2840,14 @@
 		const {
 			useRangeHeader,
 			forceRangeRequests,
+			eocdCache,
+			size,
 			options
 		} = httpReader;
 		if (useRangeHeader || forceRangeRequests) {
+			if (eocdCache && index == size - END_OF_CENTRAL_DIR_LENGTH && length == END_OF_CENTRAL_DIR_LENGTH) {
+				return eocdCache;
+			}
 			const response = await sendRequest(HTTP_METHOD_GET, httpReader, getRangeHeaders(httpReader, index, length));
 			if (response.status != 206) {
 				throw new Error(ERR_HTTP_RANGE);
@@ -2850,7 +2863,7 @@
 	}
 
 	function getRangeHeaders(httpReader, index = 0, length = 1) {
-		return Object.assign({}, getHeaders(httpReader), { [HTTP_HEADER_RANGE]: HTTP_RANGE_UNIT + "=" + index + "-" + (index + length - 1) });
+		return Object.assign({}, getHeaders(httpReader), { [HTTP_HEADER_RANGE]: HTTP_RANGE_UNIT + "=" + (index < 0 ? index : index + "-" + (index + length - 1)) });
 	}
 
 	function getHeaders({ options }) {
@@ -3545,9 +3558,10 @@
 					commentUTF8,
 					rawExtraField: directoryArray.subarray(extraFieldOffset, commentOffset)
 				});
+				const decode = getOptionValue$1(zipReader, options, "decodeText") || decodeText;
 				const [filename, comment] = await Promise.all([
-					decodeText(rawFilename, filenameUTF8 ? CHARSET_UTF8 : filenameEncoding || CHARSET_CP437),
-					decodeText(rawComment, commentUTF8 ? CHARSET_UTF8 : commentEncoding || CHARSET_CP437)
+					decode(rawFilename, filenameUTF8 ? CHARSET_UTF8 : filenameEncoding || CHARSET_CP437),
+					decode(rawComment, commentUTF8 ? CHARSET_UTF8 : commentEncoding || CHARSET_CP437)
 				]);
 				Object.assign(fileEntry, {
 					rawComment,
@@ -4151,12 +4165,13 @@
 		} else {
 			options.directory = name.endsWith(DIRECTORY_SIGNATURE);
 		}
-		const rawFilename = encodeText(name);
+		const encode = getOptionValue(zipWriter, options, "encodeText", encodeText);
+		const rawFilename = encode(name);
 		if (getLength(rawFilename) > MAX_16_BITS) {
 			throw new Error(ERR_INVALID_ENTRY_NAME);
 		}
 		const comment = options.comment || "";
-		const rawComment = encodeText(comment);
+		const rawComment = encode(comment);
 		if (getLength(rawComment) > MAX_16_BITS) {
 			throw new Error(ERR_INVALID_ENTRY_COMMENT);
 		}
@@ -4259,7 +4274,7 @@
 			zip64DiskNumberStart,
 			password,
 			rawPassword,
-			level,
+			level: !useCompressionStream && zipWriter.config.CompressionStream === UNDEFINED_VALUE ? 0 : level,
 			useWebWorkers,
 			encryptionStrength,
 			extendedTimestamp,
