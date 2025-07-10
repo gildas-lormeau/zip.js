0a9fcf02c2c7e59887968075211b84f4ced49e27
Gildas <gildas.lormeau@gmail.com>
2025-07-10 19:20:01 +0200

    add tests (see #579)

---
+++ tests/all/test-zip64-passthrough.js
@@ -0,0 +1,38 @@
+/* global Blob */
+
+import * as zip from "../../index.js";
+
+const TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
+const FILENAME = "lorem.txt";
+const BLOB = new Blob([TEXT_CONTENT], { type: zip.getMimeType(FILENAME) });
+
+export { test };
+
+async function test() {
+	zip.configure({ chunkSize: 128, useWebWorkers: true });
+	let blobWriter = new zip.BlobWriter("application/zip");
+	let zipWriter = new zip.ZipWriter(blobWriter);
+	await zipWriter.add(FILENAME, new zip.BlobReader(BLOB), { zip64: true });
+	const zipWithNoLocalZip64ExtraField = await zipWriter.close();
+	let zipReader = new zip.ZipReader(new zip.BlobReader(await blobWriter.getData()));
+	let entries = await zipReader.getEntries();
+	let data = await entries[0].getData(new zip.BlobWriter(), { passThrough: true });
+	await zipReader.close();
+	if (data.size != entries[0].compressedSize) {
+		throw new Error();
+	}
+	const signature = entries[0].signature;
+	const uncompressedSize = TEXT_CONTENT.length;
+	blobWriter = new zip.BlobWriter("application/zip");
+	zipWriter = new zip.ZipWriter(blobWriter);
+	await zipWriter.add(FILENAME, new zip.BlobReader(data), { zip64: true, passThrough: true, uncompressedSize, signature });
+	const zipWithLocalZip64ExtraField = await zipWriter.close();
+	zipReader = new zip.ZipReader(new zip.BlobReader(await blobWriter.getData()));
+	entries = await zipReader.getEntries();
+	data = await entries[0].getData(new zip.TextWriter(), { checkSignature: true });
+	await zipReader.close();
+	await zip.terminateWorkers();
+	if (data != TEXT_CONTENT || zipWithLocalZip64ExtraField.size <= zipWithNoLocalZip64ExtraField.size) {
+		throw new Error();
+	}
+}
