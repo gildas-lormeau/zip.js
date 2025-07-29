d77c5eaef46d12d18db61d29eda48887cd5611d7
Gildas <gildas.lormeau@gmail.com>
2025-07-29 18:43:08 +0200

    always terminate workers

--- tests/all/test-data-uri.js
+++ tests/all/test-data-uri.js
@@ -5,13 +5,16 @@ const FILENAME = "lorem.txt";
 export { test };
 
 async function test() {
-	zip.configure({ chunkSize: 128, useWebWorkers: true });
-	const zipWriter = new zip.ZipWriter(new zip.Data64URIWriter());
-	await zipWriter.add(FILENAME, new zip.TextReader(""));
-	const data64Uri = await zipWriter.close();
-	const zipReader = new zip.ZipReader(new zip.Data64URIReader(data64Uri));
-	const entries = await zipReader.getEntries();
-	await entries[0].getData(new zip.BlobWriter());
-	await zipReader.close();
-	await zip.terminateWorkers();
+	try {
+		zip.configure({ chunkSize: 128, useWebWorkers: true });
+		const zipWriter = new zip.ZipWriter(new zip.Data64URIWriter());
+		await zipWriter.add(FILENAME, new zip.TextReader(""));
+		const data64Uri = await zipWriter.close();
+		const zipReader = new zip.ZipReader(new zip.Data64URIReader(data64Uri));
+		const entries = await zipReader.getEntries();
+		await entries[0].getData(new zip.BlobWriter());
+		await zipReader.close();
+	} finally {
+		await zip.terminateWorkers();
+	}
 }
