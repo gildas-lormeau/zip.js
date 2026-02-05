72a3f751b0db557a57ae76cfcac6f8a712ec3c23
Gildas <gildas.lormeau@gmail.com>
2026-02-05 15:23:22 +0100

    do not resuse stream

--- tests/all/test-comments.js
+++ tests/all/test-comments.js
@@ -3,7 +3,7 @@
 import * as zip from "../../index.js";
 
 const TEXT_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
-const BLOB_STREAM = new Blob([TEXT_CONTENT]).stream();
+const BLOB = new Blob([TEXT_CONTENT]);
 
 export { test };
 
@@ -12,7 +12,7 @@ async function test() {
 		const blobWriter = new zip.BlobWriter("application/zip");
 		const zipWriter = new zip.ZipWriter(blobWriter);
 		for (let indexFile = 0; indexFile < 100; indexFile++) {
-			await zipWriter.add("file" + indexFile + ".txt", BLOB_STREAM, { comment: "comment " + indexFile });
+			await zipWriter.add("file" + indexFile + ".txt", new zip.BlobReader(BLOB), { comment: "comment " + indexFile });
 		}
 		await zipWriter.close();
 		const zipReader = new zip.ZipReader(new zip.BlobReader(await blobWriter.getData()));
