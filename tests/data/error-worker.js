308e92d6aeccd034a0813ec3ac125f489c189145
Gildas <gildas.lormeau@gmail.com>
2026-08-04 02:20:10 +0200

    make error message more informative regarding the uncaught error

--- tests/data/error-worker.js
+++ tests/data/error-worker.js
@@ -1,2 +1,2 @@
 // worker script used by test-worker-error.js that fails when evaluated
-throw new Error("simulated worker load error");
+throw new Error("simulated worker load error (expected output from test-worker-error.js)");
