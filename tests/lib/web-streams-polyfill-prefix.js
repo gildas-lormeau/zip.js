1d12b73243840d0b70ddf2a31f88f9a3f66df508
Danny Lin <danny0838@gmail.com>
2026-08-13 20:50:28 +0800

    use direct conditional import

--- tests/lib/web-streams-polyfill-prefix.js
+++
@@ -1,4 +0,0 @@
-// Backup native Streams for conditional polyfills.
-globalThis._ReadableStream = globalThis.ReadableStream;
-globalThis._WritableStream = globalThis.WritableStream;
-globalThis._TransformStream = globalThis.TransformStream;
