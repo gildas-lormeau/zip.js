6f2fc753282506b35447634a23e751410a1ca76b
Gildas <gildas.lormeau@gmail.com>
2026-08-20 13:25:31 +0200

    decode cp437 text writers and strip the byte order mark

--- dist/zip.js
+++ dist/zip.js
@@ -3288,6 +3288,96 @@
 	}
 
 	/*
+	 Copyright (c) 2022 Gildas Lormeau. All rights reserved.
+
+	 Redistribution and use in source and binary forms, with or without
+	 modification, are permitted provided that the following conditions are met:
+
+	 1. Redistributions of source code must retain the above copyright notice,
+	 this list of conditions and the following disclaimer.
+
+	 2. Redistributions in binary form must reproduce the above copyright 
+	 notice, this list of conditions and the following disclaimer in 
+	 the documentation and/or other materials provided with the distribution.
+
+	 3. The names of the authors may not be used to endorse or promote products
+	 derived from this software without specific prior written permission.
+
+	 THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+	 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+	 FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JCRAFT,
+	 INC. OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
+	 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	 LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+	 OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	 LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	 */
+
+	/* global TextDecoder */
+
+	const CP437 = "\0☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ ".split("");
+	const VALID_CP437 = CP437.length == 256;
+
+	function decodeCP437(stringValue) {
+		if (VALID_CP437) {
+			let result = "";
+			for (let indexCharacter = 0; indexCharacter < stringValue.length; indexCharacter++) {
+				result += CP437[stringValue[indexCharacter]];
+			}
+			return result;
+		} else {
+			return new TextDecoder().decode(stringValue);
+		}
+	}
+
+	/*
+	 Copyright (c) 2022 Gildas Lormeau. All rights reserved.
+
+	 Redistribution and use in source and binary forms, with or without
+	 modification, are permitted provided that the following conditions are met:
+
+	 1. Redistributions of source code must retain the above copyright notice,
+	 this list of conditions and the following disclaimer.
+
+	 2. Redistributions in binary form must reproduce the above copyright 
+	 notice, this list of conditions and the following disclaimer in 
+	 the documentation and/or other materials provided with the distribution.
+
+	 3. The names of the authors may not be used to endorse or promote products
+	 derived from this software without specific prior written permission.
+
+	 THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+	 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+	 FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JCRAFT,
+	 INC. OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
+	 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	 LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+	 OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	 LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	 */
+
+
+	function decodeText(value, encoding) {
+		return decode(value, encoding, true);
+	}
+
+	function decodeTextRemovingBOM(value, encoding) {
+		return decode(value, encoding, false);
+	}
+
+	function decode(value, encoding, ignoreBOM) {
+		if (encoding && encoding.trim().toLowerCase() == "cp437") {
+			return decodeCP437(value);
+		} else {
+			return new TextDecoder(encoding, { ignoreBOM }).decode(value);
+		}
+	}
+
+	/*
 	 Copyright (c) 2025 Gildas Lormeau. All rights reserved.
 
 	 Redistribution and use in source and binary forms, with or without
@@ -3582,7 +3672,7 @@
 			if (blob.text && utf8) {
 				return blob.text();
 			} else {
-				return new TextDecoder(encoding).decode(await blob.arrayBuffer());
+				return decodeTextRemovingBOM(new Uint8Array(await blob.arrayBuffer()), encoding);
 			}
 		}
 	}
@@ -4248,88 +4338,6 @@
 
 	function readUint8Array(reader, offset, size) {
 		return reader.readUint8Array(offset, size);
-	}
-
-	/*
-	 Copyright (c) 2022 Gildas Lormeau. All rights reserved.
-
-	 Redistribution and use in source and binary forms, with or without
-	 modification, are permitted provided that the following conditions are met:
-
-	 1. Redistributions of source code must retain the above copyright notice,
-	 this list of conditions and the following disclaimer.
-
-	 2. Redistributions in binary form must reproduce the above copyright 
-	 notice, this list of conditions and the following disclaimer in 
-	 the documentation and/or other materials provided with the distribution.
-
-	 3. The names of the authors may not be used to endorse or promote products
-	 derived from this software without specific prior written permission.
-
-	 THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
-	 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-	 FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JCRAFT,
-	 INC. OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
-	 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-	 LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-	 OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	 LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-	 */
-
-	/* global TextDecoder */
-
-	const CP437 = "\0☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ ".split("");
-	const VALID_CP437 = CP437.length == 256;
-
-	function decodeCP437(stringValue) {
-		if (VALID_CP437) {
-			let result = "";
-			for (let indexCharacter = 0; indexCharacter < stringValue.length; indexCharacter++) {
-				result += CP437[stringValue[indexCharacter]];
-			}
-			return result;
-		} else {
-			return new TextDecoder().decode(stringValue);
-		}
-	}
-
-	/*
-	 Copyright (c) 2022 Gildas Lormeau. All rights reserved.
-
-	 Redistribution and use in source and binary forms, with or without
-	 modification, are permitted provided that the following conditions are met:
-
-	 1. Redistributions of source code must retain the above copyright notice,
-	 this list of conditions and the following disclaimer.
-
-	 2. Redistributions in binary form must reproduce the above copyright 
-	 notice, this list of conditions and the following disclaimer in 
-	 the documentation and/or other materials provided with the distribution.
-
-	 3. The names of the authors may not be used to endorse or promote products
-	 derived from this software without specific prior written permission.
-
-	 THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
-	 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-	 FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JCRAFT,
-	 INC. OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
-	 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-	 LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-	 OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	 LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-	 */
-
-
-	function decodeText(value, encoding) {
-		if (encoding && encoding.trim().toLowerCase() == "cp437") {
-			return decodeCP437(value);
-		} else {
-			return new TextDecoder(encoding, { ignoreBOM: true }).decode(value);
-		}
 	}
 
 	/*
--- tests/all/test-text-writer-encoding.js
+++ tests/all/test-text-writer-encoding.js
@@ -1,27 +1,62 @@
+/* global TextEncoder */
+
 import * as zip from "../zip-lib.js";
 
 const TEXT_CONTENT = "Lorem ipsum dolor sit amet, héllo wörld, consectetuer adipiscing elit.";
 const FILENAME = "lorem.txt";
 const LATIN1_ENCODING = "iso-8859-1";
+const CP437_ENCODING = "cp437";
+const CP437_BYTES = new Uint8Array([0xC9, 0xCD, 0xBB, 0x20, 0x80, 0x81, 0x82, 0x20, 0xF7, 0xFB]);
+const CP437_TEXT = "╔═╗ Çüé ≈√";
+const UTF8_BOM = new Uint8Array([0xEF, 0xBB, 0xBF]);
 
 export { test };
 
 async function test() {
 	zip.configure({ chunkSize: 128, useWebWorkers: true });
 	const latin1Content = Uint8Array.from(TEXT_CONTENT, character => character.charCodeAt(0));
+	const utf8Content = new TextEncoder().encode(TEXT_CONTENT);
 	const zipWriter = new zip.ZipWriter(new zip.BlobWriter());
-	await zipWriter.add(FILENAME, new zip.Uint8ArrayReader(latin1Content));
+	await zipWriter.add("latin1.txt", new zip.Uint8ArrayReader(latin1Content));
+	await zipWriter.add("cp437.txt", new zip.Uint8ArrayReader(CP437_BYTES));
+	await zipWriter.add(FILENAME, new zip.Uint8ArrayReader(concat(UTF8_BOM, utf8Content)));
 	const blob = await zipWriter.close();
 	const zipReader = new zip.ZipReader(new zip.BlobReader(blob));
-	const [entry] = await zipReader.getEntries();
-	const decoded = await entry.getData(new zip.TextWriter(LATIN1_ENCODING));
-	const decodedAsUtf8 = await entry.getData(new zip.TextWriter());
+	const entries = await zipReader.getEntries();
+	const latin1Entry = entries.find(entry => entry.filename == "latin1.txt");
+	const cp437Entry = entries.find(entry => entry.filename == "cp437.txt");
+	const bomEntry = entries.find(entry => entry.filename == FILENAME);
+	const latin1 = await latin1Entry.getData(new zip.TextWriter(LATIN1_ENCODING));
+	const latin1AsUtf8 = await latin1Entry.getData(new zip.TextWriter());
+	const cp437 = await cp437Entry.getData(new zip.TextWriter(CP437_ENCODING));
+	// "utf8" is a valid encoding label but not the spelling the fast path recognizes, so it decodes
+	// through the same branch as any other named encoding
+	const bomStrippedByEncoding = await bomEntry.getData(new zip.TextWriter("utf8"));
+	const bomStrippedByDefault = await bomEntry.getData(new zip.TextWriter());
 	await zipReader.close();
 	await zip.terminateWorkers();
-	if (decoded != TEXT_CONTENT) {
-		throw new Error(`expected ${JSON.stringify(TEXT_CONTENT)}, got ${JSON.stringify(decoded)}`);
-	}
-	if (decodedAsUtf8 == TEXT_CONTENT) {
+	check(latin1, TEXT_CONTENT, "iso-8859-1 content");
+	check(cp437, CP437_TEXT, "cp437 content");
+	// the byte order mark must not reach the caller, whichever branch decodes the data
+	check(bomStrippedByEncoding, TEXT_CONTENT, "byte order mark removed when an encoding is given");
+	check(bomStrippedByDefault, TEXT_CONTENT, "byte order mark removed by default");
+	if (latin1AsUtf8 == TEXT_CONTENT) {
 		throw new Error("the utf-8 reading must differ, otherwise the encoding is never exercised");
 	}
+}
+
+function check(value, expectedValue, label) {
+	if (value != expectedValue) {
+		throw new Error(`${label}: expected ${JSON.stringify(expectedValue)}, got ${JSON.stringify(value)}`);
+	}
+}
+
+function concat(...arrays) {
+	const result = new Uint8Array(arrays.reduce((size, array) => size + array.length, 0));
+	let offset = 0;
+	arrays.forEach(array => {
+		result.set(array, offset);
+		offset += array.length;
+	});
+	return result;
 }
