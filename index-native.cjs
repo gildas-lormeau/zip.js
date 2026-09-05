3b81b8f79d2abd2bc0ac1f09afd7e933effced62
Gildas <gildas.lormeau@gmail.com>
2026-09-06 01:41:22 +0200

    bump up version

--- index-native.cjs
+++ index-native.cjs
@@ -8597,7 +8597,7 @@ function formatSupported(StreamClass, format) {
  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-const VERSION = "2.11.1";
+const VERSION = "2.11.2";
 
 /*
  Copyright (c) 2025 Gildas Lormeau. All rights reserved.
