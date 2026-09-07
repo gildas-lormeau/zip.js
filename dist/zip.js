8ab2d780e2fc2aef09fe7cc01aa91e8253cea027
Gildas <gildas.lormeau@gmail.com>
2026-09-07 15:34:58 +0200

    bump up version

--- dist/zip.js
+++ dist/zip.js
@@ -8543,7 +8543,7 @@
 	 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	 */
 
-	const VERSION = "2.11.2";
+	const VERSION = "2.11.3";
 
 	/*
 	 Copyright (c) 2025 Gildas Lormeau. All rights reserved.
