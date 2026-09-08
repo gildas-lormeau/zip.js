66f9af8a23f6c2a43d5aa69176c5fc92aa9d61f7
Gildas <gildas.lormeau@gmail.com>
2026-09-08 15:40:28 +0200

    bump up version

--- dist/zip-core-external.js
+++ dist/zip-core-external.js
@@ -9015,7 +9015,7 @@ function formatSupported(StreamClass, format) {
  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-const VERSION = "2.13.0";
+const VERSION = "2.13.1";
 
 /*
  Copyright (c) 2025 Gildas Lormeau. All rights reserved.
